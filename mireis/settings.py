1fea4f573c3ac3c6c03ca1e6af7933550d94cf11
Anderson R. Livramento <andi.devel@gmail.com>
2020-07-13 15:35:10 -0300

    protecting SECRET_KEY with environ.

--- mireis/settings.py
+++ mireis/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env('.env')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oksz#pkfiz9f%764_%xn0uwovg9)vb2iey=-9vnwim*5(bbk%w'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
