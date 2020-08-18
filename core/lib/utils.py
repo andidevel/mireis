a0903cbcfc0cc01046581ef8dc9da4c751782850
Anderson R. Livramento <andi.devel@gmail.com>
2020-08-17 21:42:21 -0300

    JSON helpers.

--- core/lib/utils.py
+++ core/lib/utils.py
@@ -3,6 +3,8 @@ import re
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 
+from django.db import models
+
 REGX_EMAIL = re.compile(r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')
 
 
@@ -17,3 +19,26 @@ def email_validate(email):
         return REGX_EMAIL.match(email) is not None
     return False
 
+
+def model_row_as_dict(model):
+    result = None
+    if isinstance(model, models.Model):
+        result = {}
+        for f in model._meta.fields:
+            value = getattr(model, f.name)
+            if value is not None:
+                result[f.name] = str(value)
+            else:
+                result[f.name] = value
+    return result
+
+
+def model_as_dict(qs):
+    result = None
+    if isinstance(qs, (models.QuerySet, list)):
+        result = []
+        for r in qs:
+            result.append(model_row_as_dict(r))
+    else:
+        result = model_row_as_dict(qs)
+    return result
